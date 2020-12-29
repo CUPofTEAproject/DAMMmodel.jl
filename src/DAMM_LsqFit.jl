c55797a0c202aad12858e43a878b171a96401960
AlexisRenchon <a.renchon@gmail.com>
2020-12-29 11:52:33 -0600

    update

--- src/DAMM_LsqFit.jl
+++ src/DAMM_LsqFit.jl
@@ -1,16 +1,4 @@
 # Least square error fit of the DAMM model to data
-using LsqFit, CSV
-
-# Load fixed param
-#EaSx = 53 
-R = 8.314472*10^-3 # Universal gas constant
-Dl = 3.17 # Diffusion coeff of substrate in liquid phase
-Sxt = 0.0125 # Soil C content
-p = 2.4*10^-2 # Fraction of soil C that is considered soluble
-Dgas = 1.67 # Diffusion coefficient of oxygen in air
-# Db = 1.53 # Soil bulk density !! DAMM complex if SWC > 0.365. old 1.5396
-Db = 1.43
-Dp = 2.52 # Soil particle density
 
 # DAMM model, !for the algorithm to work, Param are scaled to be of similar magnitude
 @. multimodel(Ind_var, Param) = (1e8*Param[1]*exp(-Param[2]/(R*(273.15+Ind_var[:, 1]))))* 
