d0dad62f5343a82d7fdc8eb0ba992dff6f3d5f78
AlexisRenchon <a.renchon@gmail.com>
2021-01-02 18:53:28 -0600

    Cleaner code

--- src/DAMM.jl
+++ src/DAMM.jl
@@ -1,17 +1,18 @@
-function DAMM(x, p) # x are indepedent variables, p are parameters
+function DAMM(x, p;  # x are independent variables, p are parameters
      # Fixed param, default values
-	R = 8.314472e-3 # Universal gas constant, kJ K-1 mol-1
-	O2ₐ = 0.209 # volume of O2 in the air, L L-1
-	BD = 1.5396 # Soil bulk density, g cm-3  
-	PD = 2.52 # Soil particle density, g cm-3
-	porosity = 1-BD/PD # total porosity 
-	pₛₓ = 2.4e-2 # Fraction of soil C that is considered soluble
-	Dₗᵢ = 3.17 # Diffusion coeff of substrate in liquid phase, dimensionless
-	Dₒₐ = 1.67 # Diffusion coefficient of oxygen in air, dimensionless
-	Sxₜₒₜ = 0.0125 #
-     # Ind. and parameters to fit
+	R = 8.314472e-3, # Universal gas constant, kJ K-1 mol-1
+	O2ₐ = 0.209, # volume of O2 in the air, L L-1
+	BD = 1.5396, # Soil bulk density, g cm-3  
+	PD = 2.52, # Soil particle density, g cm-3
+	porosity = 1-BD/PD, # total porosity 
+	pₛₓ = 2.4e-2, # Fraction of soil C that is considered soluble
+	Dₗᵢ = 3.17, # Diffusion coeff of substrate in liquid phase, dimensionless
+	Dₒₐ = 1.67, # Diffusion coefficient of oxygen in air, dimensionless
+	Sxₜₒₜ = 0.0125)
+     # Independent variables
 	Tₛ = x[:, 1]
 	θ = x[:, 2]
+     # Parameters to fit
 	Eaₛₓ = p[1]
 	αₛₓ = p[2]
 	kMₒ₂ = p[3]
@@ -26,7 +27,8 @@ function DAMM(x, p) # x are indepedent variables, p are parameters
 end
 
 # test
-x = [18.0 0.3; 22 0.22]
+x = [18.0 0.3; 22.0 0.22]
 p = [62.0, 1e8, 2.0e-3, 3.46e-8]
 DAMM(x, p)
+DAMM(x, p, Sxₜₒₜ = 0.01)
 
