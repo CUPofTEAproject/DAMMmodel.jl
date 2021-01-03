b8f693c49232ddc082d3c60e7fc2c3d78e3b14aa
AlexisRenchon <a.renchon@gmail.com>
2021-01-03 16:33:39 -0600

    Tweaks

--- src/DAMM.jl
+++ src/DAMM.jl
@@ -1,20 +1,28 @@
-# Dual Arrhenius Michealis Menten (DAMM) model, Davidson et al. 2012
-include("DAMM_param.jl")
-function DAMM(x, p; fp = defaultfp)
-	porosity = 1-fp.BD/fp.PD # total porosity
+# The Dual Arrhenius and Michealis-Menten (DAMM) model, Davidson et al. 2012
+function DAMM(x, p)
+     # Constants
+	R = 8.314472e-3 # Universal gas constant, kJ K⁻¹ mol⁻¹
+	O₂ₐ = 0.209 # Volume of O₂ in the air, L L⁻¹
+	Dₗᵢ = 3.17 # Diffusion coefficient of substrate in liquid phase, dimensionless
+	Dₒₐ = 1.67 # Diffusion coefficient of oxygen in air, dimensionless
+	pₛₓ = 0.024 # Fraction of soil carbon that is considered soluble
+	conv = 2314.8148 # Convert from mgC m⁻² hr⁻¹ to μmol CO₂ m⁻² s⁻¹
+	CtoK = 273.15 # Convert °C to Kelvin
      # Independent variables
-	Tₛ = x[:, 1]
-	θ = x[:, 2]
-     # Parameters to fit
-	Eaₛₓ = p[1]
-	αₛₓ = p[2]
-	kMₒ₂ = p[3]
-	kMₛₓ = p[4]
+	Tₛ = x[:, 1] # Soil temperature, °C
+	θ = x[:, 2] # Soil moisture, m³ m⁻³
+     # Parameters 
+	Eaₛₓ = p[1] # Activation energy, kJ mol⁻¹
+	αₛₓ = p[2] # Pre-exponential factor, mgC cm⁻³ h⁻¹
+	kMₒ₂ = p[3] # Michaelis constant for O₂, L L⁻¹
+	kMₛₓ = p[4] # Michaelis constant, gC cm⁻³
+	porosity = p[5] # 1 - soil buld density / soil particle density
+	Sxₜₒₜ = p[6] # Total soil carbon, gC cm⁻³
      # DAMM model
-	Sₓ = @. fp.Sxₜₒₜ * fp.Dₗᵢ * θ^3
-	O2 = @. fp.Dₒₐ * fp.O2ₐ * ((porosity - θ)^(4/3))
+	Vmaxₛₓ = @. (αₛₓ * exp(-Eaₛₓ/(R * (CtoK + Tₛ))))
+     	Sₓ = @. pₛₓ * Sxₜₒₜ * Dₗᵢ * θ^3
 	MMₛₓ = @. Sₓ / (kMₛₓ + Sₓ)
-	MMₒ₂ = @. O2 / (kMₒ₂ + O2)
-	Vmaxₛₓ = @. (αₛₓ * exp(-Eaₛₓ/(fp.R * (273.15 + Tₛ))))
-	Resp = @. Vmaxₛₓ * MMₛₓ * MMₒ₂ * 2314.8148 # 2314 to convert mgC hr-1 to umol s-1
+	O₂ = @. Dₒₐ * O₂ₐ * ((porosity - θ)^(4/3))
+	MMₒ₂ = @. O₂ / (kMₒ₂ + O₂)
+	Resp = @. Vmaxₛₓ * MMₛₓ * MMₒ₂ * conv 
 end
