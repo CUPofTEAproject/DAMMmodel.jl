3eaa0fad148d7a6c7255de9b4dcc26a798fffccb
AlexisRenchon <a.renchon@gmail.com>
2021-01-04 11:36:14 -0600

    Cleaning code

--- src/DAMM.jl
+++ src/DAMM.jl
@@ -6,8 +6,6 @@ function DAMM(x, p)
 	Dₗᵢ = 3.17 # Diffusion coefficient of substrate in liquid phase, dimensionless
 	Dₒₐ = 1.67 # Diffusion coefficient of oxygen in air, dimensionless
 	pₛₓ = 0.024 # Fraction of soil carbon that is considered soluble
-	conv = 2314.8148 # Convert from mgC m⁻² hr⁻¹ to μmol CO₂ m⁻² s⁻¹
-	CtoK = 273.15 # Convert °C to Kelvin
      # Independent variables
 	Tₛ = x[:, 1] # Soil temperature, °C
 	θ = x[:, 2] # Soil moisture, m³ m⁻³
@@ -19,10 +17,11 @@ function DAMM(x, p)
 	porosity = p[5] # 1 - soil buld density / soil particle density
 	Sxₜₒₜ = p[6] # Total soil carbon, gC cm⁻³
      # DAMM model
-	Vmax = @. (αₛₓ * exp(-Eaₛₓ/(R * (CtoK + Tₛ)))) # Maximum potential rate of respiration
+	Vmax = @. (αₛₓ * exp(-Eaₛₓ/(R * (273.15 + Tₛ)))) # Maximum potential rate of respiration
      	Sₓ = @. pₛₓ * Sxₜₒₜ * Dₗᵢ * θ^3 # All soluble substrate, gC cm⁻³
 	MMₛₓ = @. Sₓ / (kMₛₓ + Sₓ) # Availability of substrate factor, 0-1 
 	O₂ = @. Dₒₐ * O₂ₐ * ((porosity - θ)^(4/3)) # Oxygen concentration
 	MMₒ₂ = @. O₂ / (kMₒ₂ + O₂) # Oxygen limitation factor, 0-1
-	Resp = @. Vmax * MMₛₓ * MMₒ₂ * conv # Respiration, μmol CO₂ m⁻² s⁻¹
+	Resp = @. Vmax * MMₛₓ * MMₒ₂ # Respiration, mg C cm⁻³ hr⁻¹
+	Respc = Resp .* 2314.8148 # Respiration, μmol CO₂ m⁻² s⁻¹
 end
