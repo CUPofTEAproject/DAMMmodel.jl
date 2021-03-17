da0e4967c0f8397810fa8274f97494b865cbb0fe
AlexisRenchon <a.renchon@gmail.com>
2021-03-17 11:36:22 -0500

    up

--- src/DAMMmodel.jl
+++ src/DAMMmodel.jl
@@ -1,14 +1,6 @@
 module DAMMmodel
 
-using Makie
-using AbstractPlotting.MakieLayout
-using UnicodeFun
-using LsqFit
-using CSV
-using SparseArrays
-
 include("DAMM.jl")
-include("DAMM_Makie.jl")
-include("DAMM_LsqFit.jl")
+export DAMM
 
 end
