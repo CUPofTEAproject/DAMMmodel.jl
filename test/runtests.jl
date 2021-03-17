d01bf47428411877fca8758568f84fbc97f675d4
AlexisRenchon <a.renchon@gmail.com>
2021-03-17 13:38:28 -0500

    up

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ using Test
     # Testing DAMM.jl
 	x = [18.0 0.35; 22.0 0.22] # Ind variables test
 	p = [1e8, 62, 3.46e-8, 2.0e-3, 0.4, 0.0125] # Parameters test
-	DAMMmodel.DAMM(x, p) # DAMM test
+	DAMM(x, p) # DAMM test
 end
