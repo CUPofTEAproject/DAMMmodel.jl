6cb282e48f785bac46637e753185a42c387884f9
AlexisRenchon <a.renchon@gmail.com>
2021-12-31 09:48:57 -0600

    Project.toml and test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
 using DAMMmodel
 using Test
 
-@testset "DAMMmodel.jl" begin
-    # Write your tests here.
+@testset "DAMMmodel Tests" begin    
     # Testing DAMM.jl
 	x = [18.0 0.35; 22.0 0.22] # Ind variables test
 	p = [1e8, 62, 3.46e-8, 2.0e-3, 0.4, 0.0125] # Parameters test
-	DAMM(x, p) # DAMM test
+	@test typeof(DAMM(x, p)) == Vector{Float64} # DAMM test
 end
