0ac38ce556fbf8667e4a86fdb4868019d2309653
AlexisRenchon <a.renchon@gmail.com>
2022-02-08 17:16:15 -0600

    DAMMrepl.jl animate

--- src/demo/DAMMrepl.jl
+++ src/demo/DAMMrepl.jl
@@ -5,5 +5,13 @@ using DAMMmodel
 using Plots; unicodeplots()
 df = DAMMfdata(50)
 d = DAMMmat(df.Tₛ, df.θ, df.Rₛ, 50)
-surface(d.x, d.y.*100, d.DAMM_Matrix, colormap = :jet, border = :dotted)
+
+main() = begin
+  anim = @animate for i = -180:10:180
+    surface(d.x, d.y.*100, d.DAMM_Matrix, colormap = :jet, camera = (i, 30))
+  end
+  #gif(anim, "anim_fps5.gif", fps=5)
+end
+
+main()
 
